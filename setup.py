6937884c223ea14ddf698a4f247ab92961695aec
pradumkumar6 <pradumjaiswar790@gmail.com>
2025-02-20 00:14:59 +0530

    changes in setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name = 'youtube',
     version= '0.0.0',
-    author= 'Pradum',
+    author= 'pradum',
     author_email= 'pradumkumar0612@gmail.com',
     packages= find_packages(),
     install_requires = []
